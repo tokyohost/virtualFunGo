5ab0278041dd3e8ebbef837813ff2eac65fe2cff
tokyohost <lsyiloveyou@foxmail.com>
2026-01-13 16:19:27 +0800

    update

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/tarm/serial"
 	"io/ioutil"
 	"log"
 	"os"
@@ -14,6 +15,8 @@ import (
 const pwmDataStart = "pwm1"
 const homePath = "/sys/class/hwmon/"
 const isSerialRunning = false
+
+var lastPwmValue = -1
 
 func readPwmValue(pwmFile string) (int, error) {
 	data, err := ioutil.ReadFile(pwmFile)
@@ -30,22 +33,23 @@ func readPwmValue(pwmFile string) (int, error) {
 }
 
 func main() {
-	// 1. 启动串口监听 (路径通常是 /dev/ttyACM0 或 /dev/ttyUSB0)
 	for {
-		picoPort := FindPicoPort()
-		if picoPort == "" {
-			log.Println("等待 Pico 接入...")
-			time.Sleep(2 * time.Second)
+		// 1. 启动串口监听 (路径通常是 /dev/ttyACM0 或 /dev/ttyUSB0)
+		// 1. 获取连接对象
+		s, err := OpenPico()
+		if err != nil {
+			log.Fatal(err)
 			continue
 		}
-
+		defer s.Close() // 养成好习惯，就像关闭数据库连接一样
 		// 如果串口没在运行，则启动它
 		// 这里需要你定义一个状态变量，比如 isSerialRunning
 		if !isSerialRunning {
-			go ReadPicoSerial(picoPort)
+			go ReadPicoSerial(s)
 		}
 
-		scanHwmonDirectories()
+		//扫描pwm1 并下发给pi Pico风扇转速
+		scanHwmonDirectories(s)
 		time.Sleep(1 * time.Second)
 	}
 }
@@ -82,7 +86,7 @@ func fileExists(filename string) bool {
 	return !os.IsNotExist(err)
 }
 
-func scanHwmonDirectories() error {
+func scanHwmonDirectories(s *serial.Port) error {
 	// 获取 /sys/class/hwmon 目录下的所有子目录
 	hwmonDir := homePath
 	err := filepath.Walk(hwmonDir, func(path string, info os.FileInfo, err error) error {
@@ -106,7 +110,14 @@ func scanHwmonDirectories() error {
 					if err != nil {
 						log.Printf("Error reading PWM value: %v", err)
 					} else {
-						fmt.Printf("Current PWM value: %d\n", pwmValue)
+						if lastPwmValue == pwmValue {
+							fmt.Printf("Current PWM value: %d\n", pwmValue)
+						} else {
+							fmt.Printf("Current PWM value changed: %d\n", pwmValue)
+							lastPwmValue = pwmValue
+							//主线程发送控制指令
+							SetFanSpeed(s, pwmValue)
+						}
 					}
 				} else {
 					// 如果有错误打印错误信息
@@ -120,4 +131,15 @@ func scanHwmonDirectories() error {
 	})
 
 	return err
+}
+
+// 示例：向 Pico 发送设置转速的 JSON 指令
+func SetFanSpeed(s *serial.Port, pwmValue int) {
+	// 将 0-255 的 hwmon 值转换为 0-100 的百分比
+	percent := int((float64(pwmValue) / 255.0) * 100)
+	cmd := fmt.Sprintf("{\"set_duty\": %d}\n", percent)
+	_, err := s.Write([]byte(cmd))
+	if err != nil {
+		log.Printf("发送指令失败: %v", err)
+	}
 }
